65b1634f692070ed48363fcd36ece77f9967871e
p9 <mayurhiwale2451982@gmail.com>
2026-04-02 11:42:59 +0530

    eslint: set max length to 250

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,8 @@ export default configure(
 			}
 		},
 		rules: {
+			// Long Tailwind className lines and image URLs are common in this UI codebase
+			"@stylistic/max-len": ["warn", { code: 250, tabWidth: 1, ignoreUrls: true }],
 			// temporary
 			"prefer-const": "off",
 			// we use these for grouping
